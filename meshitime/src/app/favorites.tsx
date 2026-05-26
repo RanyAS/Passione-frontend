5de1397cf5cdfb7063ce72dcc90a396b62cf9387
SUGITANI-HARUKI <2240120@ecc.ac.jp>
2026-05-26 11:17:01 +0900

    ちょっとかいた

---
+++ meshitime/src/app/favorites.tsx
@@ -0,0 +1,18 @@
+import { ScrollView, Text, View } from "react-native";
+
+
+const favoriteRestaurants = [
+  {
+    id: 1,
+    name: "らーめん横丁",
+    image: "🍜",
+    rating: 4.2,
+    discount: "-30%",
+    price: 620,
+    isHotDeal: true,
+    bgColor: "#FFDDB0",
+  },
+
+
+  
+]
