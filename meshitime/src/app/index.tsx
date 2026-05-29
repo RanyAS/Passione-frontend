4ecd366b401487d207f0ee80ed6949efb14356e9
RanyAS <ranyanastasia15@gmail.com>
2026-05-29 10:39:55 +0900

    index.tsx を元に戻す

--- meshitime/src/app/index.tsx
+++ meshitime/src/app/index.tsx
@@ -30,37 +30,37 @@ function getDevMenuHint() {
 }
 
 export default function HomeScreen() {
-  // return (
-  //   <ThemedView style={styles.container}>
-  //     <SafeAreaView style={styles.safeArea}>
-  //       <ThemedView style={styles.heroSection}>
-  //         <AnimatedIcon />
-  //         <ThemedText type="title" style={styles.title}>
-  //           Welcome to&nbsp;Expo
-  //         </ThemedText>
-  //       </ThemedView>
+  return (
+    <ThemedView style={styles.container}>
+      <SafeAreaView style={styles.safeArea}>
+        <ThemedView style={styles.heroSection}>
+          <AnimatedIcon />
+          <ThemedText type="title" style={styles.title}>
+            Welcome to&nbsp;Expo
+          </ThemedText>
+        </ThemedView>
 
-  //       <ThemedText type="code" style={styles.code}>
-  //         get started
-  //       </ThemedText>
+        <ThemedText type="code" style={styles.code}>
+          get started
+        </ThemedText>
 
-  //       <ThemedView type="backgroundElement" style={styles.stepContainer}>
-  //         <HintRow
-  //           title="Try editing"
-  //           hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-  //         />
-  //         <HintRow title="Dev tools" hint={getDevMenuHint()} />
-  //         <HintRow
-  //           title="Fresh start"
-  //           hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-  //         />
-  //       </ThemedView>
+        <ThemedView type="backgroundElement" style={styles.stepContainer}>
+          <HintRow
+            title="Try editing"
+            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
+          />
+          <HintRow title="Dev tools" hint={getDevMenuHint()} />
+          <HintRow
+            title="Fresh start"
+            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
+          />
+        </ThemedView>
 
-  //       {Platform.OS === 'web' && <WebBadge />}
-  //     </SafeAreaView>
-  //   </ThemedView>
-  // );
-  return <RegisterScreen />;
+        {Platform.OS === 'web' && <WebBadge />}
+      </SafeAreaView>
+    </ThemedView>
+  );
+  // return <RegisterScreen />; Rany: 新規登録画面のUIを表示するために、HomeScreenコンポーネントをRegisterScreenコンポーネントに置き換えました。
 }
 
 const styles = StyleSheet.create({
