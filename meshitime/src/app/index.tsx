f5c9d1933add2cf85b59e6780e60f59ab595d1c8
RanyAS <ranyanastasia15@gmail.com>
2026-06-12 11:29:28 +0900

    Import Device and SafeAreaView components

--- meshitime/src/app/index.tsx
+++ meshitime/src/app/index.tsx
@@ -1,4 +1,6 @@
-import SearchResultsScreen from "./SearchResultsScreen";
+import * as Device from 'expo-device';
+import { Platform, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { AnimatedIcon } from '@/components/animated-icon';
 import { HintRow } from '@/components/hint-row';
